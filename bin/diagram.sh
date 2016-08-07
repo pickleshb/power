80bde67c1ba7467950aa5e6ee3f95e8a5c1ebbab
Harry Bridge <harrybridge01@gmail.com>
2016-08-07 20:33:23 +0300

    Updated shell scripts

--- bin/diagram.sh
+++ bin/diagram.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Command that generates power plan paperwork
-./diagram.py | unflatten -l 3  | dot -Tps2 | ps2pdf - > output/powerplan.pdf
+./diagram.py | unflatten -l 3  | dot -Tps2 | ps2pdf - > output/power.pdf
---
+++ bin/stats.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Command that generates network stats
+./statistics.py - > output/stats.txt

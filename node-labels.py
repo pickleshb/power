08c6d31434d91cbf479a41cae6afb02e10b84fd4
Harry Bridge <harrybridge01@gmail.com>
2016-08-09 14:42:12 +0300

    Generate node lables from node json files

--- node-labels.py
+++ node-labels.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
-# EMFCamp
-# Auto generate power distro labeles form build maps power.csv export
-# Avery L7173
+# Auto generate power distro labeles form nodes.json
+
 import labels
 import os.path
+import networkx as nx
 from reportlab.graphics import shapes
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase.pdfmetrics import registerFont
 from reportlab.lib import colors
 from reportlab.lib.units import mm
 import datetime
+import operator
+from collections import Counter
 import csv
 import urllib2
 import random
 
+from files import *
+
+# Define colours
+namedColour = {
+  'blue' : colors.HexColor('#0000ff'),
+  'tecblue': colors.HexColor('#264ba0'),
+}
+
+nodelist = []
+for node in nodes:
+  ntype = node['type'].lower()
+  if not 'incomer' in ntype or 'gen' in ntype:
+    nodelist.append(node)
+
 # config
-logo = './resources/logo-white.png'
+logo = './resources/tec-logo-power.png'
 power = './resources/powerpower.png'
-url = 'https://map.emfcamp.org/csv/Power.csv'
 
-# down load the csv and setup a dict reader
-response = urllib2.urlopen(url)
-nodes = csv.DictReader(response)
-
-# define the label paper (Avery L7173)
+# Define the label paper
 specs = labels.Specification(210, 297, 2, 5, 99, 57, corner_radius=2)
 
 # load up the fonts
@@ -32,21 +43,24 @@ registerFont(TTFont('Raleway-Bold', './resources/Raleway-Bold.ttf'))
 
 # what goes on a label
 def draw_label(label, width, height, node):
-    label.add(shapes.Image(4*mm, 4*mm, 17.49*mm, 49*mm, logo))
+    label.add(shapes.Image(0*mm, 0*mm, 17.49*mm, 53*mm, logo))
+
     label.add(shapes.String(26*mm, 125, node['name'], fontName="Raleway-Bold", fontSize=18, fillColor=colors.purple))
-    label.add(shapes.String(26*mm, 95, node['type'], fontName="Raleway", fontSize=16))
-    
-    latlong = "{}, {}".format(node['lat'], node['long'])
-    label.add(shapes.String(26*mm, 75, latlong, fontName="Raleway", fontSize=14))
+    label.add(shapes.String(26*mm, 95, node['location'], fontName="Raleway", fontSize=16))
+    label.add(shapes.String(26*mm, 75, node['type'], fontName="Raleway", fontSize=12))
+
+    # Footer
+    label.add(shapes.String(26*mm, 30, event_data['eventName'], fontName="Raleway", fontSize=12, fillColor=namedColour['tecblue']))
+    label.add(shapes.String(26*mm, 15, event_data['rigNumber'], fontName="Raleway", fontSize=12, fillColor=namedColour['tecblue']))
 
     label.add(shapes.Image(92*mm, 0.77*mm, 3.39*mm, 55.46*mm, power))
 
 sheet = labels.Sheet(specs, draw_label, border=False)
 
 # draw each node as a label
-for node in nodes:
+for node in nodelist:
     sheet.add_label(node)
 
 # save it
 sheet.save('output/node-labels.pdf')
-print("{0:d} label(s) output on {1:d} page(s).".format(sheet.label_count, sheet.page_count))
+# print("{0:d} label(s) output on {1:d} page(s).".format(sheet.label_count, sheet.page_count))
